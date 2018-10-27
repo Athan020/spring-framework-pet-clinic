bf2c33397caf0d19008ccf4986b275a5b9fda65b
Athandile <athandiletembile020@gmail.com>
2018-10-27 07:39:06 +0200

    Initial commit

---
+++ src/main/java/com/athandile/springpetclinic/SpingPetClinincApplication.java
@@ -0,0 +1,12 @@
+package com.athandile.springpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingPetClinincApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingPetClinincApplication.class, args);
+	}
+}
---
+++ src/test/java/com/athandile/springpetclinic/SpingPetClinincApplicationTests.java
@@ -0,0 +1,16 @@
+package com.athandile.springpetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpingPetClinincApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
